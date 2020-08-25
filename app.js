d1e979be57fa21e43f1e493793c73bf520b1cf99
CJ Bayron <cjdbayron@gmail.com>
2020-08-25 22:55:17 +0800

    add simple rec button

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+
+rec.addEventListener('click', () => {
+
+  	rec.disabled = true;
+
+  	setTimeout(() => {
+  		rec.disabled = false;
+  	}, 1500);
+});
+
+
 // load Onsets and Frames model from Magenta
 let transcriberModel = new mm.OnsetsAndFrames('https://storage.googleapis.com/magentadata/js/checkpoints/transcription/onsets_frames_uni');
 let modelLoaded = transcriberModel.initialize();
